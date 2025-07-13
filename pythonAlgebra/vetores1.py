0c8290897357d42b179b169f155183fc45f2017d
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2025-07-12 23:36:48 -0300

    Add files via upload

---
+++ pythonAlgebra/vetores1.py
@@ -0,0 +1,20 @@
+# Jonatan Paschoal
+# 12 de jul de 2025
+# Soma de vetores
+#-------------------------------------------
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Definição dos vetores
+u = [1,2]
+v = [2,1]
+
+# Transformndo em array
+u = np.array(u)
+v = np.array(v)
+
+
+# Soma de vetores
+z = u + v
+
+print('Vetor z = ', z)
---
+++ pythonAlgebra/vetores2.py
@@ -0,0 +1,22 @@
+# Jonatan Paschoal
+# 12 de jul de 2025
+# Lista para vetores
+#-------------------------------------------
+
+import numpy as np
+
+# Definição de um vetor
+v = np.array([3,5,7,9]) # Não esquecer [ , , ]
+
+# Lista para vetor
+lista = [3,5,66,20]
+
+v1 = np.array(lista)
+v2 = np.array([1,2,3,4])
+v3 = np.array((4,3,2,1))
+
+# Tela
+print(v)
+print(v1)
+print(v2)
+print(v3)
