b8d837f02ddf07d94ce2b5d3b89554837e362cb7
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2025-07-13 23:54:43 -0300

    Add files via upload

---
+++ pythonAlgebra/vetores9.py
@@ -0,0 +1,33 @@
+# Jonatan Paschoal
+# 13 de jul de 2025
+# Plot de vetor 2D
+#-------------------------------------------
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plotVectors(vecs, cols, alpha=0.5):
+    ''' função para plotar vetores'''
+    plt.figure()
+    plt.axvline(x=0, color='#A9A9A9', zorder=0)
+    plt.axhline(y=0, color='#A9A9A9', zorder=0)
+
+    for i in range(len(vecs)):
+        x = np.concatenate([[0,0],vecs[i]])
+        plt.quiver([x[0]],
+                   [x[1]],
+                   [x[2]],
+                   [x[3]],
+                   angles='xy', scale_units='xy', scale=1, color=cols[i],
+                  alpha=alpha)
+
+laranja = '#FF9A13'
+azul = '#1190FF'
+resultante = '#11FFFF'
+
+plotVectors([[2,3], [4,-1], [6,2]], [laranja, azul, resultante])
+
+plt.xlim(-1,7)
+plt.ylim(-2,7)
+
+plt.show()
