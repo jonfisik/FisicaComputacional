9d8a7ea2c1dc63dec67f459752252d56da42eab7
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2025-08-02 23:57:23 -0300

    Add files via upload

---
+++ pythonAlgebra/determinante8.py
@@ -0,0 +1,12 @@
+# Jonatan Paschoal
+# 02 de ago de 2025
+# Determinante de matrizes em python
+# Determinates - numpy.linalg, função para uso do (det)
+# Uso matplotlib.pyplot
+#
+#-------------------------------------------
+# Determinantes propriedades
+
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy.linalg import det
